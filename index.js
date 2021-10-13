8c6aac9414cf61651245b1f9a2c7e11549bba926
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2021-10-13 21:47:21 +0700

    [FCA-28] Test express framework

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('*/test', (req, res) => {
+    res.status(200).send("Fitness Coaching App");
+});
+
 app.get('/',(req, res) => {
     res.status(200).send('Hello World');
 });
