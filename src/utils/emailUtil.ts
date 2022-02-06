f9ce6e45bf2d23fc8ccc6a835581146839d41e8c
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2022-02-07 00:58:25 +0700

    [FCA-174] Add HTML Template to email

--- src/utils/emailUtil.ts
+++ src/utils/emailUtil.ts
@@ -24,24 +24,149 @@ export const sendVerificationEmail = (userInfo: { displayName: string, email: st
     )
 
     const transporter = createTransport();
-
     const link = config.apiHostUrl + `/auth/verifyEmail/${token}`;
+
+    const htmlContent = `
+        <!doctype html>
+        <html lang="en-US">
+        
+        <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+            <!--100% body table-->
+            <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+                style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+                <tr>
+                    <td>
+                        <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                            align="center" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                        style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:0 35px;">
+                                                <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Please verify your email</h1>
+                                                <span
+                                                    style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                                <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                                  Please click on the button below to verify your email.
+                                                </p>
+                                                <a href="${link}"
+                                                    style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">VERIFY EMAIL</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!--/100% body table-->
+        </body>
+        
+        </html>
+    `
 
     return transporter.sendMail({
         from: '"Fitness Coaching App" <fitnesscoachingapp@gmail.com>',
         to: userInfo.email,
-        subject: "Verify your email address",
-        html: `Please verify in 10 minutes by clicking this link<br><br><a href="${link}">Verify Email</a>`,
+        subject: "Fitness Coaching App - Verify Email Address",
+        html: htmlContent,
     });
 }
 
 export const sendForgetPasswordEmail = (email: string, rawPassword: string) => {
+    const htmlContent = `  
+        <!doctype html>
+        <html lang="en-US">
+        
+        <head>
+            <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+            <title>Reset Password Email Template</title>
+            <meta name="description" content="Reset Password Email Template.">
+            <style type="text/css">
+                a:hover {text-decoration: underline !important;}
+            </style>
+        </head>
+        
+        <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+            <!--100% body table-->
+            <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+                style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+                <tr>
+                    <td>
+                        <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                            align="center" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                        style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:0 35px;">
+                                                <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
+                                                    requested to reset your password</h1>
+                                                <span
+                                                    style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                                <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                                    Your new password is
+                                                </p>
+                                                <p style="color:#455056; font-size:40px;line-height:24px; margin:15pt;">
+                                                    ${rawPassword}
+                                                </p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!--/100% body table-->
+        </body>
+        
+        </html>
+    `
     const transporter = createTransport();
 
     return transporter.sendMail({
         from: '"Fitness Coaching App" <fitnesscoachingapp@gmail.com>',
         to: email,
         subject: "Fitness Coaching App - Forget Password",
-        html: `Your new password is<br><br>${rawPassword}<br>`,
+        html: htmlContent,
     });
 }
