49ae28d236fe8bfa67b343564b8177974b3ece08
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2022-04-04 01:12:24 +0700

    ✨Implement exercise/complete
    with xp calculation and poseData upload

--- src/controllers/ExerciseController.ts
+++ src/controllers/ExerciseController.ts
@@ -1,14 +1,71 @@
-import {NextFunction, Request, Response} from 'express';
-import {success} from "../utils/responseApi";
-import {ObjectId} from 'mongodb';
+import { NextFunction, Request, Response } from 'express';
+import { success } from "../utils/responseApi";
+import { ObjectId } from 'mongodb';
 import models from '../models';
+import fs from 'fs';
+import os from 'os';
+import YAML from 'yaml';
+import { uploadLocalFile } from '../utils/gcsFileUtil';
+
+const difficultyScore: {
+    [key: string]: number,
+} = {
+    EASY: 100,
+    MEDIUM: 200,
+    HARD: 300
+}
 
 export const complete = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const user = req.user!;
-        let body = req.body;
+        const user: any = req.user!
+        let body: any = req.body
+
+        // Get the course info
+        const courseInfo: any = await models.courses.findOne({ _id: new ObjectId(body.courseId) })
+        // Calculate xp = (difficulty * 10) + (score * 20)
+        const xpEarned = difficultyScore[courseInfo.difficulty] + (body.score * 20);
+
+        // TODO: Check if the user is level up
+        // TODO: Check if the user is eligible for any new achievement
+
+        const data = {
+            courseId: new ObjectId(body.courseId),
+            duration: body.duration,
+            calories: body.calories,
+            score: body.score,
+            xpEarned: xpEarned,
+            poseData: body.poseData
+        }
+
+        // Create PoseData temp file waiting for upload
+        const doc = new YAML.Document()
+        doc.contents = body.poseData
+        const tempFilePath: string = `${os.tmpdir()}/exercise_complete_tmp_${user._id}_${Date.now()}.yaml`
+
+        fs.writeFileSync(tempFilePath, doc.toString())
+
+        // Upload the temp file
+        await uploadLocalFile(tempFilePath, user._id.toString(), "poseData", "text/yaml")
+
+        const infoToInsert = {
+            userId: new ObjectId(user._id),
+            activityType: "EXERCISE",
+            timestamp: new Date(Date.now()),
+            isPublic: body.isPublic,
+            data: data,
+            reactions: [],
+            comments: []
+        }
+
+        // TODO: mongoDB insert
         
 
+        const result = {
+            levelUp: false, // TODO: Change this
+            xpEarned: xpEarned
+        }
+
+        res.status(200).send(success(res.statusCode, "The exercise data is received successfully.",))
     } catch (error) {
         next(error)
     }
