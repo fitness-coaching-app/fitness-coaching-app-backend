c9e86e750c2f645919a2773a30697c6595f2cc52
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2022-04-13 17:37:33 +0700

    ✨Implement search in users db

--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -1,15 +1,21 @@
-import {NextFunction, Request, Response} from 'express'
+import { NextFunction, Request, Response } from 'express'
 import models from '../models'
-import {error, success} from '../utils/responseApi'
+import { error, success } from '../utils/responseApi'
 
 
 export const search = async (req: Request, res: Response, next: NextFunction) => {
-	try{
-		const searchString = req.query.q;
-		console.log(searchString);
+	try {
+		const searchString = req.query.q as string;
 
-		res.status(200).send(success(res.statusCode, "Searching has been done successfully", []))
-	} catch (e){
+		const rawUserResults = await models.users.search(searchString)
+
+		const result = {
+			users: await rawUserResults.toArray(),
+			courses: []
+		}
+
+		res.status(200).send(success(res.statusCode, "Searching has been done successfully", result))
+	} catch (e) {
 		next(e)
 	}
 }
--- src/models/users.ts
+++ src/models/users.ts
@@ -1,4 +1,4 @@
-import {db} from '../utils/mongoUtil';
+import { db } from '../utils/mongoUtil';
 
 export const find = async (query: object) => {
     return await db().collection('users').find(query).toArray();
@@ -13,13 +13,29 @@ export const insertOne = async (document: object) => {
 }
 
 export const updateOne = async (query: object, newValue: object) => {
-    return await db().collection('users').updateOne(query, {$set: newValue});
+    return await db().collection('users').updateOne(query, { $set: newValue });
 }
 
 export const userExists = async (displayName: string): Promise<boolean> => {
-    return !!(await db().collection('users').findOne({displayName}));
+    return !!(await db().collection('users').findOne({ displayName }));
 }
 
 export const updateEmailVerificationComplete = async (email: string) => {
-    return await db().collection('users').updateOne({email}, {$set: {status: "SETTING_UP"}});
+    return await db().collection('users').updateOne({ email }, { $set: { status: "SETTING_UP" } });
+}
+
+export const search = async (query: string) => {
+    return await aggregate([{
+        $search: {
+            index: "usersindex",
+            regex: {
+                query: `.*${query}.*`,
+                path: ["displayName", "email"]
+            }
+        }
+    }])
+}
+
+export const aggregate = async (pipeline: object[]) => {
+    return await db().collection('users').aggregate(pipeline);
 }
