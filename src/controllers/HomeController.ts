9acd6961cf32f79e924a2d12e7ebb2385d77c863
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2022-04-22 10:15:08 +0700

    ✨Implement course fetch when call getSections

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -4,7 +4,22 @@ import models from '../models';
 
 export const getSections = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const sectionResult = await models.sections.find({})
+        let sectionResult = await models.sections.find({})
+
+        for(var i = 0;i < sectionResult.length;i++){
+            if(sectionResult[i].sectionType === "COURSE"){
+                for(var j = 0;j < sectionResult[i].data.length;j++){
+                    const courseInfo: any = (await models.courses.findOne({_id: sectionResult[i].data[j].courseId}))
+                    sectionResult[i].data[j] = {
+                        courseId: courseInfo._id,
+                        name: courseInfo.name,
+                        difficulty: courseInfo.difficulty,
+                        coverPicture: courseInfo.coverPicture,
+                        overallRating: courseInfo.overallRating || 0.0
+                    }
+                }
+            }
+        }
 
         res.status(200).send(success(res.statusCode, "Home feed sections fetched successfully", sectionResult))
     } catch (error) {
