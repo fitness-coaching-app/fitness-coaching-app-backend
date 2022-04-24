db3eae1ba08d520a2b569a6c7779fdd31dbfc911
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2022-04-24 15:02:27 +0700

    🧪 Write tests for the functions

--- __tests__/controller/activityController.test.ts
+++ __tests__/controller/activityController.test.ts
@@ -6,6 +6,7 @@ import { ObjectId } from 'mongodb';
 
 
 var accessToken = "";
+var activityId;
 
 beforeAll(async () => {
 	await mongoUtil.connect();
@@ -26,7 +27,7 @@ beforeAll(async () => {
 
 	accessToken = res.body.results.accessToken;
 
-	await mockActivity({userId: jackId})
+	activityId = (await mockActivity({userId: jackId})).insertedId;
 	await mockActivity({userId: patricId})
 }, 10000)
 
@@ -41,6 +42,95 @@ describe('GET /activity/feed', () => {
 		expect(res.statusCode).toEqual(200);
 		expect(res.body.error).toEqual(false);
 		expect(res.body.results.length).toEqual(1);
+	})
+})
+
+describe('GET /activity/feed', () => {
+	it('should return activity feed of the followers', async () => {
+		const res = await request(api)
+		.get(`/activity/feed`)
+		.set('Authorization', 'Bearer ' + accessToken)
+
+		expect(res.body.message).toEqual("Get activity feed successfully");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.error).toEqual(false);
+		expect(res.body.results.length).toEqual(1);
+	})
+})
+
+
+describe('GET /activity/{activityId}/get', () => {
+	it('should fetch activity feed according to the id', async () => {
+		const res = await request(api)
+		.get(`/activity/${activityId}/feed`)
+
+		expect(res.body.message).toEqual("Activity fetched successfully");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.error).toEqual(false);
+		expect(res.body.results._id).toEqual(activityId.toString());
+	})
+	it('should reject if the owner of the activity is private', async () => {
+		const res = await request(api)
+		.get(`/activity/${activityId}/feed`)
+
+		expect(res.body.message).toEqual("Activity fetched successfully");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.error).toEqual(false);
+		expect(res.body.results._id).toEqual(activityId.toString());
+	})
+})
+
+describe('GET /activity/{activityId}/reaction/add', () => {
+	it('should add reaction to activity', async () => {
+		const res = await request(api)
+		.get(`/activity/${activityId}/reaction/add`)
+
+		expect(res.body.message).toEqual("Reaction added successfully");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.error).toEqual(false);
+		expect(res.body.results._id).toEqual(activityId.toString());
+	})
+	it('should not duplicate the reaction from the same person', async () => {
+		const res = await request(api)
+		.get(`/activity/${activityId}/reaction/add`)
+
+		expect(res.body.message).toEqual("Reaction already added");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.error).toEqual(false);
+		expect(res.body.results._id).toEqual(activityId.toString());
+	})
+})
+
+describe('GET /activity/{activityId}/reaction/remove', () => {
+	it('should remove reaction from activity', async () => {
+		const res = await request(api)
+		.get(`/activity/${activityId}/reaction/add`)
+
+		expect(res.body.message).toEqual("Reaction removed successfully");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.error).toEqual(false);
+		expect(res.body.results._id).toEqual(activityId.toString());
+	})
+	it('should reject if the reaction is not found for the user', async () => {
+		const res = await request(api)
+		.get(`/activity/${activityId}/reaction/add`)
+
+		expect(res.body.message).toEqual("Reaction not found");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.error).toEqual(false);
+		expect(res.body.results._id).toEqual(activityId.toString());
+	})
+})
+
+describe('GET /activity/{activityId}/comment/add', () => {
+	it('should add comment to activity', async () => {
+		const res = await request(api)
+		.get(`/activity/${activityId}/reaction/add`)
+
+		expect(res.body.message).toEqual("Comment added successfully");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.error).toEqual(false);
+		expect(res.body.results._id).toEqual(activityId.toString());
 	})
 })
 
